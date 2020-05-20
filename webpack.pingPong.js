88fe62f27318ae8323fb3cc5bdc6b282299b7873
Serhii Polishchuk <spolischook@gmail.com>
2020-05-20 05:05:42 +0300

    Added webpack config and script

---
+++ webpack.pingPong.js
@@ -0,0 +1,29 @@
+const path = require('path');
+
+module.exports = {
+    entry: './src/pingPongServer.ts',
+    mode: 'development',
+    devtool: 'inline-source-map',
+    target: 'node',
+    node: {
+        // Need this when working with express, otherwise the build fails
+        __dirname: false,   // if you don't put this is, __dirname
+        __filename: false,  // and __filename return blank or /
+    },
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/,
+            },
+        ],
+    },
+    resolve: {
+        extensions: [ '.tsx', '.ts', '.js' ],
+    },
+    output: {
+        filename: 'pingPongServer.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+};
